612523c7b757d93d91180404b93210853e0a7ceb
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-15 00:17:42 +0200

    Moving and extending config to config folder

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 var infrastructure = require("infrastructure");
 
-infrastructure({
-  process_mode: "single",
-  structures: {
-    log: {engine: "log", options: {sys: true, build: true}},
-    webpack: {
-      "path":   ["bundles", "*/*.webpack.js"    ],
-      "engine":  "infrastructure-webpack/engine",
-      "loader":  "infrastructure-webpack/loader",
-
-      "config": {
-        "webpack": {
-          "watch":            true,
-          "progress":         true,
-          "buildDestination": "./public",
-          "publicPath":       "/public",
-          "sourceMap":        true,
-        }
-      }
-    }
-  }
-}, function(err, env){
+infrastructure({}, function(err, env){
 
 });

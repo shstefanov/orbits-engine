83f5e4ed6b54ed1bdc485de63d95f582da3a1051
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-15 17:12:56 +0200

    Setting up threejs engine for viewport

--- bundles/engine/controllers/MapViewportController.js
+++ bundles/engine/controllers/MapViewportController.js
@@ -1,5 +1,7 @@
 var Controller  = require("infrastructure/lib/client/Controller");
 
+const THREE = require("three");
+
 module.exports = Controller.extend("MapViewportController", {
   initOrder: 0,
   config: "viewport",
@@ -7,6 +9,51 @@ module.exports = Controller.extend("MapViewportController", {
     const app = require("app");
     this.container = document.querySelector(this.config.container);
     if(!this.container) return cb("Can't find DOM element " + this.config.container);
+    this.setViewportDimmensions();
+    this.setCameraOptions();
+    this.createRenderer();
+    this.createCamera();
+    this.createScene();
+    this.container.appendChild(this.renderer.domElement);
     cb();
+  },
+
+  setViewportDimmensions: function(){
+    const container = this.container;
+    let { width, height } = getComputedStyle(this.container);
+    width  = parseInt(width  .replace("px", "")) - 8;
+    height = parseInt(height .replace("px", "")) - 8;
+    this.viewport_dimmensions = { width, height };
+  },
+
+  setCameraOptions: function(){
+    this.camera_options = {
+      view_angle: this.config.view_angle || 45,
+      aspect:     this.viewport_dimmensions.width / this.viewport_dimmensions.height,
+      near:       this.config.near       || 0.1,
+      far:        this.config.far        || 1000,
+    };
+  },
+
+  createCamera: function(){
+    this.camera = new THREE.PerspectiveCamera(
+      this.camera_options.view_angle,
+      this.camera_options.aspect,
+      this.camera_options.near,
+      this.camera_options.far
+    );
+  },
+
+  createRenderer: function(){
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(
+      this.viewport_dimmensions.width,
+      this.viewport_dimmensions.height
+    );
+  },
+
+  createScene: function(){
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
   }
 });

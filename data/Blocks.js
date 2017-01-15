26c47c41c7f629b2b18cf7decb0fe625d1dd206f
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-15 23:55:42 +0200

    Remove unneeded comments

--- data/Blocks.js
+++ data/Blocks.js
@@ -1,6 +1,5 @@
 var MongoLayer =require("infrastructure-mongodb/MongoLayer");
-// https://docs.mongodb.com/v3.2/tutorial/build-a-2d-index/
-// https://docs.mongodb.com/v3.2/tutorial/query-a-2d-index/
+
 module.exports = MongoLayer.extend("Blocks", {
   collectionName: "Blocks",
 });

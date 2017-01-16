2647098eb5dab38923df9c7bc0788ff51e70ab27
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-16 22:46:25 +0200

    Fixing some errors in Header section

--- bundles/engine/views/sections/Header/Header.js
+++ bundles/engine/views/sections/Header/Header.js
@@ -1,7 +1,5 @@
 var App = require("App");
-var _ = require("underscore");
-
 module.exports = require("engine.lib.View").extend({
   template: require("./Header.html"),
-  style: require("./Header.less"),
+  style:    require("./Header.less"),
 });

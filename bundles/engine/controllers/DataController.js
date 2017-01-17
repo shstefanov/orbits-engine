231e571d6048906a44928cc6cdd1e2ad109a5662
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-17 10:19:25 +0200

    Adding DataController to client controllers

---
+++ bundles/engine/controllers/DataController.js
@@ -0,0 +1,10 @@
+var Controller  = require("infrastructure/lib/client/Controller");
+
+module.exports = Controller.extend("DataController", {
+  initOrder: 1,
+  init: function(options, cb){
+    var data = require("data");
+
+    cb();
+  }
+});

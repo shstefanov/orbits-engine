50daed536e6cb958ce960b3cddda4c787e8ace16
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-20 20:48:31 +0200

    formatting

--- bundles/engine/controllers/DataController.js
+++ bundles/engine/controllers/DataController.js
@@ -3,18 +3,17 @@ const Controller  = require("infrastructure/lib/client/Controller");
 module.exports = Controller.extend("DataController", {
   initOrder: 1,
   init: function(options, cb){
+    
     const app    = require("app");
     const socket = app.WebsocketController;
     const data   = require("data");
     cb();
-
 
     socket.getBlocks(null, function(err, result){
       if(err) return console.error(err);
       console.log("blocks comming");
       data.blocks.reset(result.blocks);
     });
-
 
   }
 });

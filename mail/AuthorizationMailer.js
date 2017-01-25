b6c2685f44c9015c67abb989209e90b322c8b5a0
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-01-25 21:40:39 +0200

    Change config option name

--- mail/AuthorizationMailer.js
+++ mail/AuthorizationMailer.js
@@ -1,7 +1,8 @@
 const Mailer = require("infrastructure-mail/Mailer");
 
 module.exports = Mailer.extend("AuthorizationMailer", {
-  "account": "secret.mails.auth",
-  "defaults": "secret.mails.auth-default",
+  "account":  "secret.mails.auth",
+  "defaults": "secret.mails.auth_defaults",
   "sendRegistrationVerification": "auth/verification.mustache | Hello {{username}}, please, confirm your registration"
-})
+});
+

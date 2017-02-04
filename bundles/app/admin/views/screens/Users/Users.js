586bbeafdea65acfc1125465fb35461e5affbb59
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-02-04 19:42:20 +0200

    Fetching and displaying users in admin->users screen

--- bundles/app/admin/views/screens/Users/Users.js
+++ bundles/app/admin/views/screens/Users/Users.js
@@ -1,11 +1,17 @@
-
+const UsersCollection = require("lib/models/Users/UsersCollection.js");
 module.exports = require("View").extend({
   
   template: require("./Users.html"),
   style:    require("./Users.less"),
 
   onrender: function(){
-    console.log("Users init here");
+    const app = require("app");
+    var users = new UsersCollection();
+    app.WebsocketController.getUsers({}, (err, result)=>{
+      if(err) return alert(err);
+      users.reset(result.users);
+    });
+    this.set("users", users);
   }
 
 });

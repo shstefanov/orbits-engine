961d4682b8a723f20637e3f5955fbc1327115f68
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-02-05 15:07:42 +0200

    Adding left_sidebar switcher to editor layout data

--- bundles/app/editor/views/Layout.js
+++ bundles/app/editor/views/Layout.js
@@ -6,6 +6,15 @@ function parse(name, context, cb){ cb(name.split("/").shift()); }
 module.exports = require("lib/views/BaseLayout.js").extend({
   template: require("./Layout.html"),
   style: require("./Layout.less"),
+
+  data: function(){
+    return {
+      left_sidebar: {
+        
+      }
+    };
+  },
+
   components: _.extend(
     App.bulk( require.context("./sections", true, /\.\/[^/]+\/[^\/]+\.js$/), parse ),
     App.bulk( require.context("./screens",  true, /\.\/[^/]+\/[^\/]+\.js$/), parse )

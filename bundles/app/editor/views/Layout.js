212589334072967b5d2475a0157226a50beb2e31
Stefan Stefanov <stefan@localhost.localdomain>
2017-02-11 21:42:35 +0200

    Change editor navigation to be based on routes

--- bundles/app/editor/views/Layout.js
+++ bundles/app/editor/views/Layout.js
@@ -6,18 +6,6 @@ function parse(name, context, cb){ cb(name.split("/").shift()); }
 module.exports = require("lib/views/BaseLayout.js").extend({
   template: require("./Layout.html"),
   style: require("./Layout.less"),
-
-  data: function(){
-    return {
-      left_sidebar_state: {
-        materials:  false,
-        geometries: false,
-        animations: false,
-        objects:    false,
-      }
-    };
-  },
-
   components: _.extend(
     App.bulk( require.context("./sections", true, /\.\/[^/]+\/[^\/]+\.js$/), parse ),
     App.bulk( require.context("./screens",  true, /\.\/[^/]+\/[^\/]+\.js$/), parse )

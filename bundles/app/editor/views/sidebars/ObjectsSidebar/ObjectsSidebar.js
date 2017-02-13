794a67ace96c50e124043406fc75f79b4eeed8c9
Stefan Stefanov <stefan@localhost.localdomain>
2017-02-13 22:45:33 +0200

    Adding initial data to ObjectsSidebar component

--- bundles/app/editor/views/sidebars/ObjectsSidebar/ObjectsSidebar.js
+++ bundles/app/editor/views/sidebars/ObjectsSidebar/ObjectsSidebar.js
@@ -1,4 +1,10 @@
 module.exports = require("lib/views/BaseHeaderView.js").extend({
   template: require("./ObjectsSidebar.html"),
   style:    require("./ObjectsSidebar.less"),
+
+  data: function(){
+    return {
+      search: ""
+    };
+  },
 });

a8831d1356e1e433f347a7d104d9b6e6bd201959
Stefan Stefanov <stefan@localhost.localdomain>
2017-02-13 22:47:58 +0200

    Observing and handling curentObjectModel

--- bundles/app/editor/controllers/EditorViewportController.js
+++ bundles/app/editor/controllers/EditorViewportController.js
@@ -5,7 +5,10 @@ module.exports = ThreejsViewportController.extend("EditorViewportController", {
   initOrder:    2,
   config:       "viewport",
   observe: {
-    "state.screen": "updateViewportSize"
+    "state.screen":       "updateViewportSize",
+    "currentObjectModel": "setObjectModel",
+  },
+
   resources: function(){
     const data = require("editor.data");
     return _.pick(data, [
@@ -23,5 +26,37 @@ module.exports = ThreejsViewportController.extend("EditorViewportController", {
   // },
 
   // cube_geometry: new THREE.CubeGeometry( 1,1,1 ),
+
+
+  setObjectModel: function(model){
+    if(this.objectModel){
+      if(this.objectModel === model) return;
+      else this.removeObjectModel();
+    }
+
+    if(model){
+      this.addObjectModel(require("app").get("currentObjectModel"));
+    }
+  },
+
+  removeObjectModel: function(){
+    const model = this.objectModel;
+    const mesh  = this.object_map.get(model);
+
+    delete this.objectModel;
+    this.scene.remove(mesh);
+
+    this.object_map.delete(mesh);
+    this.object_map.delete(model);
+  },
+
+  addObjectModel: function(model){
+    console.log("addObjectModel", model);
+    const mesh = this.createMeshFromModel(model);
+    if(!mesh) return;
+    this.objectModel = model;
+    this.object_map.set(mesh, model);
+    this.object_map.set(model, mesh);
   }
+
 });

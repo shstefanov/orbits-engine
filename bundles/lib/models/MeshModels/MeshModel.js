80314987879c4f17880bc263172b19bea92e9324
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-02-18 20:47:14 +0200

    Remove defaults from MeshModel

--- bundles/lib/models/MeshModels/MeshModel.js
+++ bundles/lib/models/MeshModels/MeshModel.js
@@ -1,12 +1,3 @@
 module.exports = require("lib/models/BaseModel.js").extend("MeshModel", {
-  defaults: {
-
-    material: "MeshLambertMaterial",
-    material_options: [{ color: 0xCCCCCC }],
-
-    geometry: "CubeGeometry",
-    geometry_options: [1, 1, 1]
-
-
-  }
+  
 });

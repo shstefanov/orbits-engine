440257f9b6d7e3521d2ba60ac0d0cae8f8dc14b5
Stefan Stefanov <s.h.stefanov@mail.bg>
2017-02-18 12:35:34 +0200

    Moving meshModels to resources namespace

--- bundles/app/editor/controllers/DataController.js
+++ bundles/app/editor/controllers/DataController.js
@@ -3,9 +3,10 @@ const Controller  = require("infrastructure/lib/client/Controller");
 module.exports = Controller.extend("DataController", {
   initOrder: 1,
   init: function(options, cb){
-    const app    = require("app");
-    const socket = app.WebsocketController;
-    const data   = require("editor/data.js");
+    const app       = require("app");
+    const socket    = app.WebsocketController;
+    const data      = require("editor/data.js");
+    const resources = require("editor/resources.js");
 
     Promise.all([
 
@@ -13,7 +14,7 @@ module.exports = Controller.extend("DataController", {
       new Promise(function(success, error){
         socket.getMeshModels(null, function(err, result){
           if(err) return error(err);
-          data.meshModels.reset(result.models);
+          resources.meshModels.reset(result.models);
           success(true);
         });
       }),
--- bundles/app/editor/resources.js
+++ bundles/app/editor/resources.js
@@ -1,6 +1,9 @@
 // Resources namespace
 // Contains collections
 module.exports = {
+
+  meshModels: new (require("lib/models/MeshModels/MeshModelsCollection.js")),
+
   materials: new (require("lib/models/Materials/MaterialsCollection.js"))(
     require("lib/resources/materials/common.js")
   )

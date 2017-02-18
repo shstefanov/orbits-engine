1e0825971aa24317510e98f39980fd81882ad1d9
Stefan Stefanov <stefan@localhost.localdomain>
2017-02-18 11:53:51 +0200

    Removing state defaults from MeshModel prtotype

--- bundles/lib/models/MeshModel/MeshModel.js
+++ bundles/lib/models/MeshModel/MeshModel.js
@@ -1,13 +1,5 @@
 module.exports = require("lib/models/BaseModel.js").extend("MeshModel", {
   defaults: {
-    // coordinates
-    x: 0, y: 0, z: 0,
-
-    // rotation
-    rx: 0, ry: 0, rz: 0,
-
-    // scale
-    sx: 1, sy: 1, sz: 1,
 
     material: "MeshLambertMaterial",
     material_options: [{ color: 0xCCCCCC }],

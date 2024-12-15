11a568108691f5b874b7d80df7624cbef98318c8
Stefan Stefanov <s.h.stefanov@mail.bg>
2024-12-15 11:07:08 +0000

    Cleaning up code

--- lib/OrbitsRenderer.jsx
+++ lib/OrbitsRenderer.jsx
@@ -2,20 +2,10 @@ import React, { useState, useEffect, createContext, useContext } from "react";
 
 import * as THREE from 'three';
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
 const rendererContext    = createContext();
 export const useRenderer = () => useContext(rendererContext);
 
 const RendererProvider = rendererContext.Provider;
-
-console.log("RendererProvider", RendererProvider);
-
 
 function getSize(canvas){
     let { width, height } = getComputedStyle(canvas);
